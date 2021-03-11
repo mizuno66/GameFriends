b1f58788ecb8ac1f657f79c0abf6c9ca4cd9d802
Mizuno66 <kouta126@hotmail.co.jp>
2021-03-12 00:09:40 +0900

    コミット漏れ追加

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/index.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8000/graphql/',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
